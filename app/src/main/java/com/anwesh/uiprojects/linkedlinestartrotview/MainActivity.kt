33ac8032e61c18af926530f34e24b11b230cc9df
Anwesh43 <anweshthecool0@gmail.com>
2019-07-03 12:28:39 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinestartrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinestartrotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinestartrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linestartrotview.LineStartRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineStartRotView.create(this)
     }
 }
