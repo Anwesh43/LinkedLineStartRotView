a1384a556f0a2ca0e07eee34475911938aecd99f
Anwesh43 <anweshthecool0@gmail.com>
2019-07-03 11:36:06 +0530

    adding libraries and constants

--- linestartrotview/src/main/java/com/anwesh/uiprojects/linestartrotview/LineStartRotView.kt
+++ linestartrotview/src/main/java/com/anwesh/uiprojects/linestartrotview/LineStartRotView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.linestartrotview
  * Created by anweshmishra on 03/07/19.
  */
 
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
