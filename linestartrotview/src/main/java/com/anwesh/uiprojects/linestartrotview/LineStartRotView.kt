34669336d24363e02dd6a398ff022f49966d7c40
Anwesh43 <anweshthecool0@gmail.com>
2019-07-03 11:58:30 +0530

    adding LineStartRotView class

--- linestartrotview/src/main/java/com/anwesh/uiprojects/linestartrotview/LineStartRotView.kt
+++ linestartrotview/src/main/java/com/anwesh/uiprojects/linestartrotview/LineStartRotView.kt
@@ -64,3 +64,20 @@ fun Canvas.drawLSRNode(i : Int, scale : Float, paint : Paint) {
     }
 }
 
+class LineStartRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
